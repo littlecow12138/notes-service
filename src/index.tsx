37fe98f3bd352cd862c084a4d3324ff1516bd089
Jankin Sun <jingshuang.sun@goodwe.com>
2022-09-07 15:08:01 +0800

    Config micro app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./public-path";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const render = (props: any) => {
+  const { container } = props;
+  ReactDOM.render(
+    <BrowserRouter
+      basename={(window as any).__POWERED_BY_QIANKUN__ ? "/notes" : "/"}
+    >
+      <App />
+    </BrowserRouter>,
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+};
+
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log("[react16] react app bootstraped");
+}
+
+export async function mount(props: any) {
+  console.log("[react16] props from main framework", props);
+  render(props);
+}
+
+export async function unmount(props: any) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
